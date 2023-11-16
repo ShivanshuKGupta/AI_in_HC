cdc1af02468d01116a72d6f6b66167197bc193e5
Shivanshu Gupta <shivanshukgupta@gmail.com>
2023-11-16 16:11:29 +0530

    New update

---
+++ update.bat
@@ -0,0 +1,19 @@
+@REM This script is for updating any changes to the git repo
+@REM Change the below name with your branch name
+@echo off
+set branchName=testbranch
+echo %branchName%
+echo "Before running this script make sure that you have a branch named %branchName% and have all your changes commited only in that branch."
+@echo on
+git checkout %branchName%
+git add .
+git commit
+git checkout master
+git pull origin master
+git merge %branchName%
+git push -u origin master
+git branch -d %branchName% 
+git branch %branchName% 
+git checkout %branchName%
+
+@REM For any kind of conflict in merging you need to manage it manually.
